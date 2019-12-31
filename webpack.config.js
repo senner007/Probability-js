8884ee18c8e1c998b93003d2dfcb4bb24076248c
nig <nig@beepanalytics.com>
2019-12-31 16:29:55 +0100

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const NodemonPlugin = require('nodemon-webpack-plugin'); // Ding
 const path = require('path');
  
 module.exports = {
+    mode : "development",
     output: {
         path: path.resolve('./dist'),
         filename: 'bundled.js',
