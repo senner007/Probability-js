d2f4d5dcb141441c31fef2a4a2e37c73d70d12c5
nig <nielshtg@hotmail.com>
2021-12-29 15:37:21 +0100

    wip

--- __tests__/non-exclusive-events.test.js
+++ __tests__/non-exclusive-events.test.js
@@ -1,33 +1,37 @@
 import Outcome from "../src/permutationsWithRepetitions";
 
+const N_THROWS = 3;
+const N_RANGE = 6
+
 const threeDices = Outcome({
-       title: "throw dice 3 times",
-       mode : "compute",
-       range : [1, 2, 3, 4, 5, 6], // input range (eg. dice)
-       times : 3, // roll n times
-       showData : false
+    title: `throw dice ${N_THROWS} times`,
+    mode: "compute",
+    range: Array.from({ length: N_RANGE }, (n, i) => i + 1), // input range (eg. dice)
+    times: N_THROWS, // roll n times
+    showData: false
 });
 
-const [ rollingOne ] = threeDices('rolling at least one 1', arr => {
+const [rollingOne] = threeDices('rolling at least one 1', arr => {
     return arr.includes(1)
 });
 
-const [ rollingTwo ] = threeDices('rolling at least one 2', arr => {
+const [rollingTwo] = threeDices('rolling at least one 2', arr => {
     return arr.includes(2)
 })
 
-const [ rollingOneAndTwo ] = threeDices('rolling at least one 1 and at least one 2', arr => {
+const [rollingOneAndTwo] = threeDices('rolling at least one 1 and at least one 2', arr => {
     return arr.includes(1) && arr.includes(2)
 })
 
-const [ rollingOneOrTwo ] = threeDices('rolling a 1 or a 2', arr => {
+
+const [rollingOneOrTwo] = threeDices('rolling a 1 or a 2', arr => {
     return arr.includes(1) || arr.includes(2)
 })
 
 describe('For 3 dices, rolling at least one 1', () => {
-       test('Probability is equal to: 1 - ((5/6)**3)', () => {
-              expect(rollingOne).toEqual(1 - ((5/6)**3));
-       });
+    test('Probability is equal to: Total - (N_RANGE -1 / N_RANGE) ** N_THROWS) = 1 - ((5/6)**3)', () => {
+        expect(rollingOne).toEqual(1 - ((N_RANGE - 1) / N_RANGE) ** N_THROWS);
+    });
 })
 
 describe('For 3 dices, rolling a 1 or a 2', () => {
@@ -36,6 +40,6 @@ describe('For 3 dices, rolling a 1 or a 2', () => {
     });
 
     test('Probability is equal to: 0.7037037037037037', () => {
-       expect(rollingOneOrTwo).toEqual(0.7037037037037037);
-   });
+        expect(rollingOneOrTwo).toEqual(0.7037037037037037);
+    });
 })
