dd13be797f59b92fc28e2e49d93fe8b42774ed56
nig <nielshtg@hotmail.com>
2021-12-31 02:07:44 +0100

    wip

--- __tests__/non-exclusive-events.test.js
+++ __tests__/non-exclusive-events.test.js
@@ -1,4 +1,5 @@
 import Outcome from "../src/permutationsWithRepetitions";
+import { hasDuplicates } from "../helpers/helpers";
 
 const N_THROWS = 3;
 const N_RANGE = 6
@@ -27,7 +28,57 @@ const [rollingOneOrTwo] = threeDices('rolling a 1 or a 2', arr => {
     return arr.includes(1) || arr.includes(2)
 })
 
+const [rollingAllEqual] = threeDices('all dices being equal', arr => {
+    var obj = {};
+    for (let n of arr) {
+        obj[n] = obj[n] ? obj[n] + 1 : 1;
+        if (obj[n] === 3) return true;
+    }
+})
+
+const [rollingAllDifferent] = threeDices('all dices being different', arr => {
+    return !hasDuplicates(arr)
+})
+
+const [rollingTwoSameAndOneDifferent] = threeDices('rolling two same and one different', arr => {
+    var obj = {};
+    for (let n of arr) {
+        obj[n] = obj[n] ? obj[n] + 1 : 1;
+    }
+    return Object.values(obj).some(entry => entry == 2)
+})
+
+describe('For 3 dices, all dices being different', () => {
+
+    test('Probability is equal to: 120/216', () => {
+        expect(rollingAllDifferent).toEqual(120/216);
+    });
+})
+
+describe('For 3 dices, all dices being equal', () => {
+
+    test('Probability is equal to: 6/216', () => {
+        expect(rollingAllEqual).toEqual(6/216);
+    });
+})
+
+describe('For 3 dices, rolling two same and one different', () => {
+
+    test('Probability is equal to:  1 - rollingAllEqual - rollingAllDifferent = 90/216', () => {
+        expect(rollingTwoSameAndOneDifferent).toBeCloseTo(1 - rollingAllEqual - rollingAllDifferent, 5);
+    });
+
+    test('Probability is equal to: 90/216', () => {
+        expect(rollingTwoSameAndOneDifferent).toEqual(90 / 216);
+    });
+})
+
 describe('For 3 dices, rolling at least one 1 and at least one 2', () => {
+
+    test('Probability is equal to: rollingTwoSameAndOneDifferent / 3 = 30/216', () => {
+        expect(rollingOneAndTwo).toEqual(rollingTwoSameAndOneDifferent /3);
+    });
+
     test('Probability is equal to: 30/216', () => {
         expect(rollingOneAndTwo).toEqual(30 / 216);
     });
